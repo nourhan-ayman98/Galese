f6793973a0c58b89bd9319aef77f19434559079b
nourhan-ayman98 <nourhankhan98@gmail.com>
2021-02-14 14:44:11 +0200

    first Commit

---
+++ Component/Welcome_page.js
@@ -0,0 +1,41 @@
+import { Dimensions, StyleSheet, Text, View,StatusBar} from 'react-native';
+import React ,{Component} from 'react-native';
+
+
+
+class Welcome_page extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+            <View style={styles.header}>
+                <Text> Header </Text>
+            </View>
+            <View style={styles.footer}>
+                <Text> footer </Text>
+            </View>
+            </View>
+        )
+    }
+}
+
+
+export default Welcome_page;
+const styles = StyleSheet.create({
+    container: {
+      flex: 1, 
+      backgroundColor: '#009387'
+    },
+    header: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingVertical: 50,
+        paddingHorizontal: 30
+    }
+});
